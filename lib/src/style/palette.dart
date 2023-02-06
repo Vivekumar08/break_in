5658d3080b149f3299b28b64a3d9c3256ce3d9c5
Vivekumar01 <vivekumar2003bsr@gmail.com>
2023-02-06 14:26:11 +0530

    palette/fonts

---
+++ lib/src/style/palette.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static Color get primary => const Color(0xff563129);
+  static Color get secondary => const Color(0xFFA5375F);
+  static Color get background => const Color(0xFFE7D9B1);
+  static Color get text => const Color(0xff1E232C);
+  static Color get background_2 => const Color(0xFFFFFFFF);
+  static Color get inputfield => const Color(0xFFF7F8F9);
+  static Color get stroke => const Color(0xffE8ECF4);
+  static Color get iconscol => const Color(0xff6A707C);
+  static Color get greynormal => const Color(0xff8391A1);
+}
