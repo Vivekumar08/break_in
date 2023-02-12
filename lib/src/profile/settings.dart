616f05ec803f4300f582cb2d25246a54e2245917
Vivekumar01 <vivekumar2003bsr@gmail.com>
2023-02-12 19:49:13 +0530

    settings

--- lib/src/profile/settings.dart
+++ lib/src/profile/settings.dart
@@ -4,21 +4,21 @@ import 'package:flutter/material.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
-  Widget _settingsCategories(String text, VoidCallback onPressed) => 
-  Padding(
-    padding: const EdgeInsets.symmetric(horizontal:22.0),
-    child:ListTile(
-        title: Text(text, style: Fonts.poppins),
-        horizontalTitleGap: 0,
-        dense: true,
-        trailing: const Icon(
-          Icons.chevron_right,
-          size: 32,
+  Widget _settingsCategories(String text, VoidCallback onPressed) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 22.0),
+        child: ListTile(
+          title: Text(text, style: Fonts.poppins),
+          horizontalTitleGap: 0,
+          dense: true,
+          trailing: const Icon(
+            Icons.chevron_right,
+            size: 32,
+          ),
+          shape: UnderlineInputBorder(
+            borderSide: BorderSide(color: Palette.greyNormal),
+          ),
         ),
-        shape: UnderlineInputBorder(
-          borderSide: BorderSide(color: Palette.greyNormal),
-        ),
-      ),);
+      );
 
   @override
   Widget build(BuildContext context) {
